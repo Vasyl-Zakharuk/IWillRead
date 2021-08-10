1af410d8d2948142eea4675ab3111aa8f41d0bb4
Vasyl Zakharuk <zakharukuv@gmail.com>
2021-08-10 20:10:59 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { AddTodo } from './src/AddTodo'
+import { Navbar } from './src/Navbar'
+import {Todo} from './src/Todo'
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos(todos.concat([ newTodo ]))
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
+  return (
+    <View>
+      <Navbar title='I wiil read' />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <ScrollView>
+          {todos.map(todo => {
+            return <Todo todo={todo} key={todo.id} />
+          }) }
+        </ScrollView>
+      </View>  
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20
+   }
+})
